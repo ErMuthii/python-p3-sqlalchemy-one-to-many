6f31eecc91623644943943fb38d0f0f844c139ab
ErMuthii <er.muthii@gmail.com>
2024-02-17 14:58:27 +0300

    done

---
+++ lib/migrations/versions/762438e3a572_create_game_model.py
@@ -0,0 +1,24 @@
+"""Create Game Model
+
+Revision ID: 762438e3a572
+Revises: faebd12f9523
+Create Date: 2024-02-17 14:38:07.453460
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '762438e3a572'
+down_revision = 'faebd12f9523'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
